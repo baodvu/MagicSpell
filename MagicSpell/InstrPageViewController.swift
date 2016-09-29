6b2923e539617f621dbf6cb165bcd3d1f457aa60
Bao Vu <baodvu@gmail.com>
2016-09-28 23:57:41 -0400

    Removed "Open Keyboard Settings" button and edited the instructions accordingly as iOS 10 no longer supports this feature.

--- MagicSpell/InstrPageViewController.swift
+++ MagicSpell/InstrPageViewController.swift
@@ -13,12 +13,8 @@ class InstrPageViewController: UIViewController{
     
     static let nextScreen = SwitchKeyboardViewController()
     
-    @IBOutlet weak var openKeyboardSettingsButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        openKeyboardSettingsButton.layer.borderColor = UIColor.darkGray.cgColor
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,10 +24,4 @@ class InstrPageViewController: UIViewController{
             appDelegate.window?.rootViewController = myView
         }
     }
-    
-    @IBAction func openKeyboardSettings(_ sender: UIButton) {
-        let keyboardSettingsURL = URL(string: "prefs:root=General&path=Keyboard/KEYBOARDS")
-        UIApplication.shared.openURL(keyboardSettingsURL!)
-    }
-    
 }
